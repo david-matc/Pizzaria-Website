21f61b3ceddb16226ed28859301b8d9c4312352d
mrinaltewatia <68030602+mrinaltewatia@users.noreply.github.com>
2021-12-02 03:47:23 +0530

    Update events.js

--- main/scripts/events.js
+++ main/scripts/events.js
@@ -1,25 +1,24 @@
 //form element events
 document.querySelector('#fName').addEventListener("blur", function() {
     if (orderInfo.fName.value !== "") {
-        fNameError.innerHTML = "";
+        fNameError.textContent = "";
     }
 });
 document.querySelector('#lName').addEventListener("blur", function() {
     if (orderInfo.lName.value !== "") {
-        lNameError.innerHTML = "";
+        lNameError.textContent = "";
     }
 });
 document.querySelector('#address').addEventListener("blur", function() {
     if (orderInfo.address.value !== "") {
-        addressError.innerHTML = "";
+        addressError.textContent = "";
     }
 });
 document.querySelector('#postal').addEventListener("blur", function() {
     if (orderInfo.postal.value !== "") {
-        postalError.innerHTML = "";
+        postalError.textContent = "";
     }
 });
-
 
 
 // document.orderInfo.addEventListener("submit", validate);
