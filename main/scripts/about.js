15d2607c803c9e5ffa87d46e491f62c57b8c41f0
david-matc <matc0023@algonquinlive.com>
2021-12-05 13:56:49 -0500

    added css
    added media queries
    added background colours

--- main/scripts/about.js
+++ main/scripts/about.js
@@ -6,9 +6,16 @@ tt.setProductInfo(APPLICATION_NAME, APPLICATION_VERSION);
 
 const white_water_brewery = {lng: -76.886082, lat: 45.632623};
 
-var map = tt.map({
-  key: api_key,
-  container: 'map-div',
-  center: white_water_brewery,
-  zoom: 12
+
+window.addEventListener("load", function(){
+  var map = tt.map({
+    key: api_key,
+    container: 'map-div',
+    center: white_water_brewery,
+    zoom: 12
+  });
+
 });
+
+//there are console errors for using this library locally.
+// The solution would be to host it on a secure server
