9f3e4ec813ce79ca059e30d3fccb10b6f477ae73
mrinaltewatia <68030602+mrinaltewatia@users.noreply.github.com>
2021-12-06 20:47:53 +0530

    Add files via upload

---
+++ scripts/event.js
@@ -0,0 +1,26 @@
+var pepperoni = document.querySelector("#pepperoni")
+var meatLovers = document.querySelector("#meatLovers")
+var vegetarian = document.querySelector("#vegetarian")
+var canadian = document.querySelector("#canadian")
+var hawaiian = document.querySelector("#hawaiian")
+var deluxe = document.querySelector("#deluxe")
+document.querySelector('#fName').addEventListener("blur", function() {
+    if (orderInfo.fName.value !== "") {
+        fNameError.textContent = "";
+    }
+});
+document.querySelector('#lName').addEventListener("blur", function() {
+    if (orderInfo.lName.value !== "") {
+        lNameError.textContent = "";
+    }
+});
+document.querySelector('#address').addEventListener("blur", function() {
+    if (orderInfo.address.value !== "") {
+        addressError.textContent = "";
+    }
+});
+document.querySelector('#postal').addEventListener("blur", function() {
+    if (orderInfo.postal.value !== "") {
+        postalError.textContent = "";
+    }
+});
---
+++ scripts/validate.js
@@ -0,0 +1,106 @@
+var delivery = document.querySelector('#delivery');
+var pickup = document.querySelector('#pickup');
+var fName = document.querySelector('#fName');
+var lName = document.querySelector('#lName');
+var totalCost = document.querySelector('#totalCost');
+var address = document.querySelector('#address');
+var postal = document.querySelector('#postal');
+// var pepperoni = document.querySelector("#pepperoni")
+// var meatLovers = document.querySelector("#meatLovers")
+// var vegetarian = document.querySelector("#vegetarian")
+// var canadian = document.querySelector("#canadian")
+// var hawaiian = document.querySelector("#hawaiian")
+// var deluxe = document.querySelector("#deluxe")
+
+/*
+Returns the total value of items selected
+ */
+function getCheckedItemsValue() {
+    let val = 0
+        // get all object keys in an array
+    const keys = Object.keys(window.itemsList)
+        // loop through keys
+    keys.forEach((key) => {
+        const isChecked = document.querySelector(`#${key}`).checked
+        if (isChecked) {
+            val += window.itemsList[key].price
+        }
+    })
+    return val
+}
+
+function validate(e) {
+    e.preventDefault;
+    getCheckedItemsValue()
+        //creating boolean value for returning
+        // If pickup or delivery arent checked/ pops an error
+    var valid = true;
+    if (pickup.checked == false || delivery.checked == false) {
+        document.querySelector('#fieldsetError').textContent = "Select either of the options";
+        valid = false;
+    }
+    // If pickup or delivery are checked/ removes the error
+    if (pickup.checked !== false || delivery.checked !== false) {
+        document.querySelector('#fieldsetError').textContent = "";
+        valid = false;
+    }
+
+    // validates if pickup is checked and atleast first and last name is entered
+    if ((pickup.checked !== false)) {
+        valid = true;
+    }
+    // form wont validate if nothing is selected
+    if (totalCost.textContent == '0') {
+        valid = false;
+        document.querySelector('#totalCostError').textContent = 'Add something to cart'
+    }
+
+    // if the total cost of the items != 0 and  delivery or pickup NOT selected, then return false
+    if (totalCost.textContent !== 0 && delivery.value != "" || pickup.value != "") {
+        // if either the first or last name are blank, dont submit
+        if (fName.value == "" || lName.value == "") {
+            if (fName.value == "") {
+                //adding the errorspan messages
+                document.querySelector('#fNameError').textContent = 'Please enter a first name';
+            }
+            if (lName.value == "") {
+                document.querySelector('#lNameError').textContent = 'Please enter a last name';
+            }
+            valid = false;
+        }
+
+        //if the delivery option was selected
+        if (delivery.checked == true) {
+            //if no address, alert an error
+            if (address.value == "") {
+                document.querySelector('#addressError').textContent = 'Please enter an address';
+            }
+            //if no postal code, alert an error
+            if (postal.value == "") {
+                document.querySelector('#postalError').textContent = 'Please enter a postal code';
+            }
+            valid = false;
+        }
+        // if delivery is selected and all the required boxes are filled, form validates.
+        if (delivery.checked == true) {
+            if (fName.value != "" && lName.value !== "" && address.value !== "" && postal.value !== "") {
+                valid = true;
+            }
+        }
+
+        // if the pickup option was selected
+        if (pickup.checked == true) {
+            address.value = '';
+            postal.value = '';
+            document.querySelector('#addressError').textContent = '';
+            document.querySelector('#postalError').textContent = '';
+            document.querySelector('#addressError').textContent = '';
+        }
+    }
+    if (valid) {
+        alert("Success");
+        return valid;
+    } else {
+        return false;
+    }
+}
