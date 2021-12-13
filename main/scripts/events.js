f102c8f1376f495a585c5b598c9741ba792051ef
mrinaltewatia <68030602+mrinaltewatia@users.noreply.github.com>
2021-12-13 05:34:28 +0530

    Update events.js

--- main/scripts/events.js
+++ main/scripts/events.js
@@ -1,25 +1,32 @@
-//form element events
+var pepperoni = document.querySelector("#pepperoni")
+var meatLovers = document.querySelector("#meatLovers")
+var vegetarian = document.querySelector("#vegetarian")
+var canadian = document.querySelector("#canadian")
+var hawaiian = document.querySelector("#hawaiian")
+var deluxe = document.querySelector("#deluxe")
+var costError = document.querySelector('#totalCostError')
+
 document.querySelector('#fName').addEventListener("blur", function() {
     if (orderInfo.fName.value !== "") {
-        fNameError.innerHTML = "";
+        fNameError.textContent = "";
     }
 });
 document.querySelector('#lName').addEventListener("blur", function() {
     if (orderInfo.lName.value !== "") {
-        lNameError.innerHTML = "";
+        lNameError.textContent = "";
     }
 });
 document.querySelector('#address').addEventListener("blur", function() {
     if (orderInfo.address.value !== "") {
-        addressError.innerHTML = "";
+        addressError.textContent = "";
     }
 });
 document.querySelector('#postal').addEventListener("blur", function() {
     if (orderInfo.postal.value !== "") {
-        postalError.innerHTML = "";
+        postalError.textContent = "";
     }
 });
 
-
-
-// document.orderInfo.addEventListener("submit", validate);
+function onDeliveryTypeChange() {
+    document.querySelector('#fieldsetError').textContent = ""
+}
